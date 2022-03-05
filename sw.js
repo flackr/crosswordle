cec63f042d2c274bc92295259ab25ffb16879a44
Robert Flack <flackr@gmail.com>
2022-03-05 11:36:06 -0500

    Allow word lists to use StaleWhileRevalidate.

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
 workbox.routing.registerRoute(
+  ({url}) => url.pathname.startsWith('/third_party/aspell6/'),
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
+workbox.routing.registerRoute(
   () => true,
   new workbox.strategies.NetworkFirst()
 );
